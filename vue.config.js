e76ca448661a29738ec3c27c535ff947cb6c8c76
Rutger Willems <rut.willems@gmail.com>
2020-02-04 13:20:52 +0200

    remove stupid favicons

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,14 @@
 // vue.config.js
 module.exports = {
   pwa: {
-    themeColor: '#000000'
+    themeColor: '#000000',
+    iconPaths: {
+      favicon32: false,
+      favicon16: false,
+      appleTouchIcon: false,
+      maskIcon: false,
+      msTileImage: false
+    }
   },
   css: {
     sourceMap: true
